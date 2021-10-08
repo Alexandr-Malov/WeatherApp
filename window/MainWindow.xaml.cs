0f313f99535f64e8593ee2a7c6ea680750dd84b3
Sanya2020618 <48901045+Sanya2020618@users.noreply.github.com>
2021-10-08 17:12:03 +0300

    Исправление ошибок

--- window/MainWindow.xaml.cs
+++ window/MainWindow.xaml.cs
@@ -3,12 +3,10 @@ using System;
 using System.Net;
 using System.Windows;
 using System.Windows.Media.Imaging;
-using WeatherApp.window;
-using WeatherApp;
 using static WeatherApp.Models.WeatherModel;
 using System.Text;
-using System.Linq;
-using System.Web;
+using System.Timers;
+using System.Threading.Tasks;
 
 namespace WeatherApp
 {
@@ -30,32 +28,38 @@ namespace WeatherApp
         private string ApiKey = "badd09470b6f3c13dcd793207e86b4a5";
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(town.Text == "")
+            if (town.Text == "")
             {
-                MessageBox.Show("Введите город!","WeatherApp");
+                MessageBox.Show("Введите город!", "WeatherApp");
             }
             else
             {
                 using (WebClient web = new WebClient())
                 {
-
-                    string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&lang=ru&appid={1}&units={2}", town.Text, ApiKey, Metric);//
-                    var json = web.DownloadString(url);
-                    Info = JsonConvert.DeserializeObject<root>(json);
-                    Uri resourceUri = new Uri("https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png", UriKind.Absolute);
-                    weatherimage.Source = new BitmapImage(resourceUri);
-                    town.Text = UnicodeToUTF8(Info.name);
-                    temp.Text = Info.main.temp.ToString();
-                    weather.Text = UnicodeToUTF8(Info.weather[0].description);
-                    sunrise.Text = ConvertFromUnixTimestamp(Info.sys.sunrise + Info.timezone).ToString();
-                    sunset.Text = ConvertFromUnixTimestamp(Info.sys.sunset + Info.timezone).ToString();
-                    humidity.Text = Info.main.humidity.ToString();
-                    pressure.Text = (Info.main.pressure * 0.75).ToString();
-                    windspeed.Text = Info.wind.speed.ToString();
+                    try
+                    {
+                        string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&lang=ru&appid={1}&units={2}", town.Text, ApiKey, Metric);//
+                        var json = web.DownloadString(url);
+                        Info = JsonConvert.DeserializeObject<root>(json);
+                        Uri resourceUri = new Uri("https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png", UriKind.Absolute);
+                        weatherimage.Source = new BitmapImage(resourceUri);
+                        town.Text = UnicodeToUTF8(Info.name);
+                        temp.Text = Info.main.temp.ToString();
+                        weather.Text = UnicodeToUTF8(Info.weather[0].description);
+                        sunrise.Text = ConvertFromUnixTimestamp(Info.sys.sunrise + Info.timezone).ToString();
+                        sunset.Text = ConvertFromUnixTimestamp(Info.sys.sunset + Info.timezone).ToString();
+                        humidity.Text = Info.main.humidity.ToString();
+                        pressure.Text = (Info.main.pressure * 0.75).ToString();
+                        windspeed.Text = Info.wind.speed.ToString();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message + "\nВозможна ошибка в правописании города!","Ошибка");
+                    }
+                   
                 }
             }
         }
-
         void GetWeather()
         {
             using (WebClient web = new WebClient())
